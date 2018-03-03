3d9eb5086645ca3a1dd9897879f59844691899af
Rafaela Azevedo <rafaela.azevedosouza@gmail.com>
2018-03-03 23:25:36 +0000

    Added constants and improved assertion and login

---
+++ features/step_definitions/pages/constants.js
@@ -0,0 +1,3 @@
+exports.CONSTANTS = {
+    TIMEOUT : 5000,
+}
--- features/step_definitions/pages/login-page.js
+++ features/step_definitions/pages/login-page.js
@@ -1,6 +1,10 @@
 import assert from 'assert';
+import { CONSTANTS } from './constants';
 
 class LoginPage {
+
+    ////////* Elements *////////
+    ////////////////////////////
 
     get username() {
         return browser.element('#usernameOrEmail');
@@ -18,16 +22,24 @@ class LoginPage {
         return browser.element('.login-link');
     }
 
+    get suspendedWarning() {
+        return browser.element('.is-error');
+    }
+    
+    ////////* Elements *////////
+    ////////////////////////////
+
     submitLogin(user, pass) {
         this.username.setValue(user);
         this.submit.click();
+        this.password.waitForEnabled(CONSTANTS.TIMEOUT);
         this.password.setValue(pass);
         this.submit.click();
     }
 
     assertLogin() {
-        const title = browser.getTitle();
-        assert.equal(title, 'Log In — WordPress.com');
+        this.suspendedWarning.waitForExist(CONSTANTS.TIMEOUT);
+        assert.equal(this.suspendedWarning.isVisible(), true);
     }
 
     openLogin() {
